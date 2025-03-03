fb4ea1b4db5d331cafbdc66c5c19031a85ef8fed
Osemudiamhen Ughu <oseughu@gmail.com>
2025-03-03 12:30:06 +0000

    change alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': resolve(__dirname, './resources/js'),
+            '@/*': resolve(__dirname, './resources/js/*'),
         },
     },
 });
