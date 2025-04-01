be4b17ab0b80d63d342cf6825bc5828e5f7929ca
Osemudiamhen Ughu <oseughu@gmail.com>
2025-04-01 20:22:38 +0100

    fix app header layout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+
         $middleware->web(append: [
             HandleAppearance::class,
             HandleInertiaRequests::class,
