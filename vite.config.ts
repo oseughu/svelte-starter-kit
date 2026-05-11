60fc90ec76b6a540804bb4bcea27a7047da30532
Osemudiamhen Ughu <oseughu@gmail.com>
2026-05-11 09:49:55 +0100

    remove wayfinder plugin from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import inertia from '@inertiajs/vite';
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
@@ -20,8 +19,5 @@ export default defineConfig({
         inertia(),
         tailwindcss(),
         svelte(),
-        wayfinder({
-            formVariants: true,
-        }),
     ],
 });
